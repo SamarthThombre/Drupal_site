31c6621637a903b959ca7cef7395d49c6365d0de
SamarthThombre <samthombre91@gmail.com>
2026-06-17 18:20:42 +0530

    home page update

---
+++ web/sites/default/files/php/twig/6a31b0d08bd14_dropbutton-wrapper.html.t_r1LQTCEsvKUzve3pjb6sBWL2y/mCqUztj7B_RtFgP3WjA95-oSeqzE_oV5LEX2wd8yeWQ.php
@@ -0,0 +1,136 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\CoreExtension;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+use Twig\TemplateWrapper;
+
+/* core/modules/system/templates/dropbutton-wrapper.html.twig */
+class __TwigTemplate_cd6b2f3fc11d6b433f6720629acebe7a extends Template
+{
+    private Source $source;
+    /**
+     * @var array<string, Template>
+     */
+    private array $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->extensions[SandboxExtension::class];
+    }
+
+    protected function doDisplay(array $context, array $blocks = []): iterable
+    {
+        $macros = $this->macros;
+        // line 12
+        if ((($tmp = ($context["children"] ?? null)) && $tmp instanceof Markup ? (string) $tmp : $tmp)) {
+            // line 13
+            yield "  <div class=\"dropbutton-wrapper\" data-drupal-ajax-container>
+    <div class=\"dropbutton-widget\">";
+            // line 14
+            yield $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, ($context["children"] ?? null), "html", null, true);
+            yield "</div>
+  </div>";
+        }
+        $this->env->getExtension('\Drupal\Core\Template\TwigExtension')
+            ->checkDeprecations($context, ["children"]);        yield from [];
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getTemplateName(): string
+    {
+        return "core/modules/system/templates/dropbutton-wrapper.html.twig";
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function isTraitable(): bool
+    {
+        return false;
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getDebugInfo(): array
+    {
+        return array (  48 => 14,  45 => 13,  43 => 12,);
+    }
+
+    public function getSourceContext(): Source
+    {
+        return new Source("{#
+/**
+ * @file
+ * Default theme implementation for a dropbutton wrapper.
+ *
+ * Available variables:
+ * - children: Contains the child elements of the dropbutton menu.
+ *
+ * @ingroup themeable
+ */
+#}
+{% if children %}
+  <div class=\"dropbutton-wrapper\" data-drupal-ajax-container>
+    <div class=\"dropbutton-widget\">{{- children -}}</div>
+  </div>
+{%- endif %}
+", "core/modules/system/templates/dropbutton-wrapper.html.twig", "/var/www/html/web/core/modules/system/templates/dropbutton-wrapper.html.twig");
+    }
+    
+    public function ensureSecurityChecked(): void
+    {
+        if ($this->sandbox->isSandboxed($this->source)) {
+            $this->checkSecurity();
+        }
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = ["if" => 12];
+        static $filters = ["escape" => 14];
+        static $functions = [];
+
+        try {
+            $this->sandbox->checkSecurity(
+                [0 => "if"],
+                [0 => "escape"],
+                [],
+                $this->source
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
---
+++ web/sites/default/files/php/twig/6a31b0d08bd14_region--header.html.twig_DTQiELfH69t0BgG3dF8qMDz6Y/ONMAk9CyvpBNEyLTZYBlwR2-2PHQhxdM8WfYEaTI2Z0.php
@@ -0,0 +1,133 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\CoreExtension;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+use Twig\TemplateWrapper;
+
+/* themes/icar_theme/templates/region--header.html.twig */
+class __TwigTemplate_1dfc42d04a80357806c7a00f0c0182a1 extends Template
+{
+    private Source $source;
+    /**
+     * @var array<string, Template>
+     */
+    private array $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->extensions[SandboxExtension::class];
+    }
+
+    protected function doDisplay(array $context, array $blocks = []): iterable
+    {
+        $macros = $this->macros;
+        // line 4
+        if ((($tmp = ($context["content"] ?? null)) && $tmp instanceof Markup ? (string) $tmp : $tmp)) {
+            // line 5
+            yield "  <div class=\"icar-header-wrapper\">
+    <div class=\"icar-header-content\">
+      ";
+            // line 7
+            yield $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, ($context["content"] ?? null), "html", null, true);
+            yield "
+    </div>
+  </div>
+";
+        }
+        $this->env->getExtension('\Drupal\Core\Template\TwigExtension')
+            ->checkDeprecations($context, ["content"]);        yield from [];
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getTemplateName(): string
+    {
+        return "themes/icar_theme/templates/region--header.html.twig";
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function isTraitable(): bool
+    {
+        return false;
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getDebugInfo(): array
+    {
+        return array (  49 => 7,  45 => 5,  43 => 4,);
+    }
+
+    public function getSourceContext(): Source
+    {
+        return new Source("{# 
+  This template wraps the main Header region, aligning the logos and text side-by-side.
+#}
+{% if content %}
+  <div class=\"icar-header-wrapper\">
+    <div class=\"icar-header-content\">
+      {{ content }}
+    </div>
+  </div>
+{% endif %}
+", "themes/icar_theme/templates/region--header.html.twig", "/var/www/html/web/themes/icar_theme/templates/region--header.html.twig");
+    }
+    
+    public function ensureSecurityChecked(): void
+    {
+        if ($this->sandbox->isSandboxed($this->source)) {
+            $this->checkSecurity();
+        }
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = ["if" => 4];
+        static $filters = ["escape" => 7];
+        static $functions = [];
+
+        try {
+            $this->sandbox->checkSecurity(
+                [0 => "if"],
+                [0 => "escape"],
+                [],
+                $this->source
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
